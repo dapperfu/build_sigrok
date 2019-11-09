09e38bdc1146b4de956ddb9a051f99d1bf6d4e25
jed <jed@m6700.lan>
2019-11-09 01:26:12 -0500

    .

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+git submodule update --init --depth=1
+git submodule foreach git fetch --all
+git submodule foreach git checkout master
 
 cd libserialport
-
 ./autogen.sh
 ./configure --prefix=/opt/sigrok
 make -j8
